b6e3fea7c4f75ef359a81fe44d7a684e441fe332
cfiguer055 <cfiguer055@gmail.com>
2022-08-07 17:37:38 -0700

    NoteDatavase and Converters. Create Room Repo

---
+++ app/src/main/java/com/example/scriptr/model/NoteDatabase.java
@@ -0,0 +1,52 @@
+package com.example.scriptr.model;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+
+
+@Database(entities = {Folder.class,Note.class}, version = 1)
+@TypeConverters({Converters.class})
+public abstract class NoteDatabase extends RoomDatabase {
+
+    public abstract FolderDAO folderDAO();
+    public abstract NoteDAO noteDAO();
+
+
+
+    // Singleton Pattern
+    private static NoteDatabase instance;
+
+    public static synchronized NoteDatabase getInstance(Context context) {
+        if(instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class, "note_datavase")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
+        }
+        return instance;
+    }
+
+
+
+    // Callback
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            // Insert data when database is created...
+
+
+
+        }
+    };
+}
---
+++ app/src/main/java/com/example/scriptr/model/ScriptrRepository.java
@@ -0,0 +1,4 @@
+package com.example.scriptr.model;
+
+public class ScriptrRepository {
+}
